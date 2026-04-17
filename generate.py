2fe35aaa8bc556f4f4c0c30cbd207a9cf8a3d3b5
Seggan <seggan21@gmail.com>
2026-04-16 21:27:13 -0400

    Only kubes this time

--- generate.py
+++ generate.py
@@ -41,9 +41,6 @@ for item_file in (assets_dir / "items").glob("*.json"):
             model = model[len("minecraft:block/"):]
             valid_item_models[item_file.stem] = model
 
-with open(data_dir / "items.json", "w") as f:
-    json.dump(valid_item_models, f)
-
 models_dir = data_dir / "models"
 models_dir.mkdir()
 
@@ -61,23 +58,25 @@ def merge_maps(map1, map2):
             result[key] = value
     return result
 
-def merge_parents(model_data: dict):
+def flatten_parents(model_data: dict, stop_at: str | None = None):
     if "parent" in model_data:
         parent: str = model_data["parent"]
-        parent = parent.removeprefix("minecraft:")
-        if not parent.startswith("block/"):
-            raise ValueError(f"Unsupported parent model: {parent}")
-        parent = parent[len("block/"):]
-        parent_file = block_models_assets_dir / f"{parent}.json"
-        parent_data = merge_parents(json.loads(parent_file.read_text()))
-        return merge_maps(parent_data, { key: value for key, value in model_data.items() if key != "parent" })
+        if parent != stop_at:
+            parent = parent.removeprefix("minecraft:")
+            if not parent.startswith("block/"):
+                raise ValueError(f"Unsupported parent model: {parent}")
+            parent = parent[len("block/"):]
+            parent_file = block_models_assets_dir / f"{parent}.json"
+            parent_data = flatten_parents(json.loads(parent_file.read_text()), stop_at=stop_at)
+            return merge_maps(parent_data, { key: value for key, value in model_data.items() if key != "parent" })
     return model_data
 
 def resolve_textures(model_data: dict):
     textures: dict[str, str] = model_data["textures"]
+    final = {}
     for element in model_data["elements"]:
-        for face in element["faces"].values():
-            texture = face["texture"]
+        for face, spec in element["faces"].items():
+            texture = spec["texture"]
             while texture.startswith("#"):
                 texture = textures[texture[1:]]
 
@@ -85,7 +84,7 @@ def resolve_textures(model_data: dict):
             if not texture.startswith("block/"):
                 raise ValueError(f"Unsupported texture: {texture}")
             texture = texture[len("block/"):]
-            face["texture"] = texture
+            final[face] = texture
 
             texture_file = assets_dir / "textures" / "block" / f"{texture}.png"
             if not texture_file.exists():
@@ -93,18 +92,27 @@ def resolve_textures(model_data: dict):
             texture_dest = textures_dir / f"{texture}.png"
             if not texture_dest.exists():
                 shutil.copy(texture_file, texture_dest)
-    model_data.pop("textures")
-    return model_data
+    return final
 
-for model in valid_item_models.values():
+invalid_items = []
+for item, model in valid_item_models.items():
     try:
         model_file = block_models_assets_dir / f"{model}.json"
         model_data = json.loads(model_file.read_text())
-        model_data = merge_parents(model_data)
-        model_data = resolve_textures(model_data)
-        with open(models_dir / f"{model}.json", "w") as f:
-            json.dump(model_data, f)
+        model_data = flatten_parents(model_data, stop_at="block/cube")
+        if "parent" in model_data and model_data["parent"] == "block/cube":
+            model_data = flatten_parents(model_data)
+            textures = resolve_textures(model_data)
+            with open(models_dir / f"{model}.json", "w") as f:
+                json.dump(textures, f)
+        else:
+            invalid_items.append(item)
     except Exception as e:
+        raise e
         print(f"Error processing model {model}: {e}")
 
 shutil.rmtree(data_dir / "client")
+
+valid_item_models = { item: model for item, model in valid_item_models.items() if item not in invalid_items }
+with open(data_dir / "items.json", "w") as f:
+    json.dump(valid_item_models, f)
